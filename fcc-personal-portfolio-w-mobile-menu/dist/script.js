563304380df8deba33713dfd9769487b81edbd42
egsch <61172180+egsch@users.noreply.github.com>
2020-05-29 14:56:29 -0600

    Add files via upload

---
+++ fcc-personal-portfolio-w-mobile-menu/dist/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  if ($(window).width() < 390) {
+    $(".nav-link").hide();
+    $("#button").text("Menu");
+  }
+});
+$("#button").click(function() {
+  if ($(".nav-link").is(":visible")) {
+    $(".nav-link").hide();
+    $("#button").text("Menu");
+  } else {
+    $(".nav-link").show();
+    $("#button").text("Close");
+  };
+});
+$(".nav-link").click(function(){
+  if ($(window).width() < 390) {
+  $(".nav-link").hide();
+  $("#button").text("Menu");
+  }
+})
+$( window ).resize(function() {
+   if ($(window).width() > 390) {
+    $(".nav-link").show();
+  } else {
+    $(".nav-link").hide();
+}});
